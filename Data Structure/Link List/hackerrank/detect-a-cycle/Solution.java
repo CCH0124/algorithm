b4b3ebd243b5cb20bda889da873ff87023be441d
CCH0124 <cchong0124@gmail.com>
2023-10-16 22:39:41 +0800

    add Detect a Cycle

---
+++ Data Structure/Link List/hackerrank/detect-a-cycle/Solution.java
@@ -0,0 +1,25 @@
+/*
+Detect a cycle in a linked list. Note that the head pointer may be 'null' if the list is empty.
+
+A Node is defined as: 
+    class Node {
+        int data;
+        Node next;
+    }
+*/
+
+boolean hasCycle(Node head) {
+    Node slow = head;
+    Node fast = head;
+    while(slow != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow == null || fast == null) {
+            return false;
+        }
+        if (slow == fast) {
+            return true;
+        }
+    }
+    return false;
+}
