383ca05de21ec0e0235bde705e0d7057279456d9
CCH0124 <cchong0124@gmail.com>
2025-02-09 21:23:00 +0800

    feat: add merge sort example

--- Data Structure/java/app/src/main/java/org/example/clrs/chapter02/sort/Merge.java
+++ Data Structure/java/app/src/main/java/org/example/clrs/chapter02/sort/Merge.java
@@ -1,6 +1,57 @@
 package org.example.clrs.chapter02.sort;
 
 public class Merge {
+    public static int countInversions(int[] arr, int left, int right) {
+        if (left >= right) return 0;
+        int middle = (left + right) / 2;
+        var l = countInversions(arr, left, middle);
+        var r = countInversions(arr, middle + 1 , right);
+        return l + r + mergeCountInversions(arr, left, middle, right);
+
+    }
+
+    public static int mergeCountInversions(int[] arr, int left, int mid, int right) {
+        int l = mid - left + 1;
+        int r = right - mid;
+        int[] leftTemp = new int[l];
+        int[] rightTemp = new int[r];
+        for (int i = 0; i < l; i++) {
+            leftTemp[i] = arr[left + i];
+        }
+        for (int i = 0; i < r; i++) {
+            rightTemp[i] = arr[mid + 1 + i];
+        }
+        int i = 0;
+        int j = 0;
+        int inversions = 0;
+        int k = left;
+        while (i < l && j < r) {
+            if (leftTemp[i] <= rightTemp[j]) {
+                arr[k] = leftTemp[i];
+                i++;
+            } else {
+                arr[k] = rightTemp[j];
+                j++;
+                inversions += (l - i);
+            }
+            k++;
+        }
+        // 複製左半邊剩餘的元素
+        while (i < l) {
+            arr[k] = leftTemp[i];
+            i++;
+            k++;
+        }
+        // 複製右半邊剩餘的元素
+        while (j < r) {
+            arr[k] = rightTemp[j];
+            j++;
+            k++;
+        }
+        
+        return inversions;
+    }
+
     public static void mergeSort(int[] arr, int left, int right) {
         if (left < right) {
             int middle = (left + right) / 2;
--- Data Structure/java/app/src/test/java/org/example/clrs/chapter02/sort/MergeTest.java
+++ Data Structure/java/app/src/test/java/org/example/clrs/chapter02/sort/MergeTest.java
@@ -1,6 +1,7 @@
 package org.example.clrs.chapter02.sort;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,5 +11,21 @@ public class MergeTest {
         var init = new int [] {5, 3, 2, 4, 4, 7, 1, 2};
         Merge.mergeSort(init, 0, init.length - 1);
         assertArrayEquals(init, new int [] {1, 2, 2, 3, 4, 4, 5, 7});
+    }
+
+    @Test
+    public void mergeCountInversionsTest() {
+        var init = new int [] {2, 3, 8, 6, 1};
+        var init2 = new int [] {4, 3, 2, 1};
+        var init3 = new int [] {2, 4, 1, 3, 5};
+        var init4 = new int [] {2, 3, 4, 5, 6};
+        int countInversions = Merge.countInversions(init, 0, init.length - 1);
+        int countInversions2 = Merge.countInversions(init2, 0, init2.length - 1);
+        int countInversions3 = Merge.countInversions(init3, 0, init3.length - 1);
+        int countInversions4 = Merge.countInversions(init4, 0, init4.length - 1);
+        assertEquals(5, countInversions);
+        assertEquals(6, countInversions2);
+        assertEquals(3, countInversions3);
+        assertEquals(0, countInversions4);
     }
 }
